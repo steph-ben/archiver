4d96aae9ff985a12725dc326115bf5391ec68a41
Steph Bench <stephane.benchimol@mfi.fr>
2020-05-12 15:57:15 +0200

    Working on

--- archiver/__init__.py
+++ archiver/__init__.py
@@ -1,8 +1,9 @@
-import celery
 import shutil
-
+import logging
 from pathlib import Path
 from datetime import datetime, timedelta
+
+logger = logging.getLogger(__name__)
 
 
 class Archiver:
@@ -12,13 +13,19 @@ class Archiver:
     base_directory = None
     default_subdir: str = "default"
 
-    def __init__(self, base_directory: Path):
+    def __init__(self, base_directory: str):
         self.base_directory = Path(base_directory)
 
-        if not self.archive_base_directory.exists():
-            self.archive_base_directory.mkdir(parents=True)
+        if not self.base_directory.exists():
+            self.base_directory.mkdir(parents=True)
 
-    def archive(self, fp: Path, subdir=self.default_subdir):
-        dest_fp = self.base_directory / subdir
+    def archive(self, fp: Path, subdir=default_subdir, unlink=False):
+        dest_fp = self.base_directory / subdir / fp.name
         logger.info(f"Archiving {fp} to {dest_fp.absolute()}")
-        fp.rename(dest_fp)
+
+        if not dest_fp.parent.exists():
+            dest_fp.parent.mkdir(parents=True)
+
+        shutil.copy(str(fp), str(dest_fp))
+        if unlink:
+            fp.unlink()
